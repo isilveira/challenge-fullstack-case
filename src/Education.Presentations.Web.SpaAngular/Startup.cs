3e2f389b9f67c934cddd2d4515cbacc2f7e1df4f
Ítalo Silveira <italobrian@gmail.com>
2020-10-12 04:03:23 -0300

    add api endpoints and migrations

--- src/Education.Presentations.Web.SpaAngular/Startup.cs
+++ src/Education.Presentations.Web.SpaAngular/Startup.cs
@@ -1,3 +1,4 @@
+using Education.Core.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
 
 namespace Education.Presentations.Web.SpaAngular
 {
@@ -20,6 +22,10 @@ namespace Education.Presentations.Web.SpaAngular
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var assembly = typeof(Startup).GetTypeInfo().Assembly;
+
+            services.AddMiddleware(Configuration, assembly);
+
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
